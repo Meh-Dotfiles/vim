ac95317c635abab39a05043e89030a5b25a1e76a
Sergi <Sergi030@users.noreply.github.com>
2017-07-04 00:36:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+VIM=~/.dotfiles/vim
 env git clone --depth=1 https://github.com/Meh-Dotfiles/vim.git $VIM || {
     printf "Error: git clone of vim repo failed\n"
         exit 1
  }
+ bash $VIM/configure-vim.sh

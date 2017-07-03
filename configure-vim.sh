3c12f0adc8f41344bf58dc58f2550b7f0bd0718c
Sergi <Sergi030@users.noreply.github.com>
2017-07-04 00:33:15 +0200

    Update configure-vim.sh

--- configure-vim.sh
+++ configure-vim.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 VIM=~/.dotfiles/vim
 
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim-orig
+fi
+
 env git clone --depth=1 https://github.com/Meh-Dotfiles/vim.git $VIM || {
     printf "Error: git clone of oh-my-zsh repo failed\n"
         exit 1

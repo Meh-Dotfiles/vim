100ddcb59daf6b8f85022a657052c3bb750b327e
Sergi <Sergi030@users.noreply.github.com>
2017-07-04 00:35:18 +0200

    Update configure-vim.sh

--- configure-vim.sh
+++ configure-vim.sh
@@ -5,11 +5,5 @@ if [ -d ~/.vim ]; then
     mv ~/.vim ~/.vim-orig
 fi
 
-env git clone --depth=1 https://github.com/Meh-Dotfiles/vim.git $VIM || {
-    printf "Error: git clone of vim repo failed\n"
-        exit 1
- }
-
-
 ln -sfv $VIM/vimrc ~/.vimrc
 ln -sfv $VIM/.vim ~/
